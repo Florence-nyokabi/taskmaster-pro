c109011d7931784515afb252022def81949741d5
Florence-nyokabi <florencenyokabiwangui@gmail.com>
2025-06-19 16:14:24 +0300

    SaaS Task Management App

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,17 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
+import { getFirestore } from "firebase/firestore";
+import { getMessaging } from "firebase/messaging";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZTVAHKjdgWSUFPHSZlP2EfDOihk2t3-M",
-  authDomain: "taskmasterpro-80e57.firebaseapp.com",
-  projectId: "taskmasterpro-80e57",
-  storageBucket: "taskmasterpro-80e57.firebasestorage.app",
-  messagingSenderId: "1058088035160",
-  appId: "1:1058088035160:web:64ad3a201b9d91d8c77b2d",
-  measurementId: "G-KPN5ENY42Y"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const messaging = getMessaging(app);
+const auth = getAuth(app);
+
+// Export the initialized services
+export { app, analytics, db, messaging, auth };
